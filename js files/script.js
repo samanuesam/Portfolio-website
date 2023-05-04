91c14f849e7aa0b302b15247a3f5443bc4ddd4a1
samanuesam <91001903+samanuesam@users.noreply.github.com>
2023-05-04 08:52:22 +0530

    Add files via upload

---
+++ js files/script.js
@@ -0,0 +1,11 @@
+let h=window.innerWidth;
+console.log(h)
+if(h<=992){
+ function closee(){
+    document.getElementById("navmenu").style.display="none"
+    
+ }
+ function openn(){
+    document.getElementById("navmenu").style.display="block"
+ }
+}
