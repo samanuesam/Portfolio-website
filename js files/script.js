e9b7b702f74a8612bd4566eff21a7725cf86ad92
samanuesam <91001903+samanuesam@users.noreply.github.com>
2023-06-20 18:47:26 +0530

    Update script.js

--- js files/script.js
+++ js files/script.js
@@ -9,3 +9,20 @@ if(h<=992){
     document.getElementById("navmenu").style.display="block"
  }
 }
+function reveal() {
+   var reveals = document.querySelectorAll(".reveal");
+ 
+   for (var i = 0; i < reveals.length; i++) {
+     var windowHeight = window.innerHeight;
+     var elementTop = reveals[i].getBoundingClientRect().top;
+     var elementVisible = 150;
+ 
+     if (elementTop < windowHeight - elementVisible) {
+       reveals[i].classList.add("active");
+     } else {
+       reveals[i].classList.remove("active");
+     }
+   }
+ }
+ 
+ window.addEventListener("scroll", reveal);
